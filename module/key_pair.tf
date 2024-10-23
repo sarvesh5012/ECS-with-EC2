da8b26bfd9676815531a320920c06f0bae512569
sarvesh5012 <114138465+sarvesh5012@users.noreply.github.com>
2024-10-23 11:46:47 +0530

    Update key_pair.tf

--- module/key_pair.tf
+++ module/key_pair.tf
@@ -2,11 +2,27 @@
 ## Create a public and private key pair for login to the EC2 Instances
 ########################################################################################################################
 
+
+
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
 resource "aws_key_pair" "default" {
   key_name   = "${var.namespace}_KeyPair_${var.environment}"
-  public_key = var.public_ec2_key
+  public_key = tls_private_key.example.public_key_openssh
 
   tags = {
     Scenario = var.scenario
   }
+}
+
+output "private_key_pem" {
+  value     = tls_private_key.example.private_key_pem
+  sensitive = true
+}
+
+output "key_name" {
+  value = aws_key_pair.default.key_name
 }
