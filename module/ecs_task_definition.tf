9136b0d66f621e6df910112f75c97bee9b977a51
sarvesh5012 <114138465+sarvesh5012@users.noreply.github.com>
2024-10-24 17:16:48 +0530

    Update ecs_task_definition.tf

--- module/ecs_task_definition.tf
+++ module/ecs_task_definition.tf
@@ -36,3 +36,44 @@ resource "aws_ecs_task_definition" "default" {
     Scenario = var.scenario
   }
 }
+
+
+# Fargate
+
+resource "aws_ecs_task_definition" "default" {
+  family                   = "${var.namespace}_ECS_TaskDefinition_${var.environment}"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  cpu                      = var.cpu_units
+  memory                   = var.memory
+
+  container_definitions = jsonencode([
+    {
+      name         = var.service_name
+      image        = var.image_uri
+      cpu          = var.cpu_units
+      memory       = var.memory
+      essential    = true
+      portMappings = [
+        {
+          containerPort = var.container_port
+          hostPort      = var.container_port
+          protocol      = "tcp"
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options   = {
+          "awslogs-group"         = aws_cloudwatch_log_group.log_group.name
+          "awslogs-region"        = var.region
+          "awslogs-stream-prefix" = "${var.service_name}-log-stream-${var.environment}"
+        }
+      }
+    }
+  ])
+
+  tags = {
+    Scenario = var.scenario
+  }
+}
