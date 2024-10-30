d55acd7014f59551bac0862da9b5a7e12873b4de
sarvesh5012 <114138465+sarvesh5012@users.noreply.github.com>
2024-10-30 21:24:26 +0530

    Update ecs_service.tf

--- module/ecs_service.tf
+++ module/ecs_service.tf
@@ -7,7 +7,7 @@ resource "aws_ecs_service" "service" {
   name                               = "${var.namespace}_ECS_Service_${var.environment}"
   iam_role                           = aws_iam_role.ecs_service_role.arn
   cluster                            = aws_ecs_cluster.default.id
-  task_definition                    = aws_ecs_task_definition.default.arn
+  task_definition                    = aws_ecs_task_definition.default[0].arn
   desired_count                      = var.ecs_task_desired_count
   deployment_minimum_healthy_percent = var.ecs_task_deployment_minimum_healthy_percent
   deployment_maximum_percent         = var.ecs_task_deployment_maximum_percent
@@ -52,7 +52,7 @@ resource "aws_ecs_service" "service_fargate" {
   count = var.launch_type == "fargate" ? 1 : 0
   name                               = "${var.namespace}_ECS_Service_${var.environment}"
   cluster                            = aws_ecs_cluster.default.id
-  task_definition                    = aws_ecs_task_definition.default_fargate.arn
+  task_definition                    = aws_ecs_task_definition.default_fargate[0].arn
   desired_count                      = var.ecs_task_desired_count
   deployment_minimum_healthy_percent = var.ecs_task_deployment_minimum_healthy_percent
   deployment_maximum_percent         = var.ecs_task_deployment_maximum_percent
