5ae78d902af3601fe892f9cf178b6b79d96b8fe6
sarvesh5012 <114138465+sarvesh5012@users.noreply.github.com>
2024-10-30 21:31:46 +0530

    Update ecs_service.tf

--- module/ecs_service.tf
+++ module/ecs_service.tf
@@ -1,13 +1,10 @@
-########################################################################################################################
-## Creates ECS Service
-########################################################################################################################
-
+# EC2 ECS Service
 resource "aws_ecs_service" "service" {
   count = var.launch_type == "ec2" ? 1 : 0
   name                               = "${var.namespace}_ECS_Service_${var.environment}"
   iam_role                           = aws_iam_role.ecs_service_role.arn
   cluster                            = aws_ecs_cluster.default.id
-  task_definition                    = aws_ecs_task_definition.default[0].arn
+  task_definition                    = aws_ecs_task_definition.default[0].arn  # Indexed for EC2
   desired_count                      = var.ecs_task_desired_count
   deployment_minimum_healthy_percent = var.ecs_task_deployment_minimum_healthy_percent
   deployment_maximum_percent         = var.ecs_task_deployment_maximum_percent
@@ -37,22 +34,19 @@ resource "aws_ecs_service" "service" {
   }
 }
 
+# Output for EC2 ECS Service
 output "ecs_service_name" {
-  value = aws_ecs_service.service.name
+  value = aws_ecs_service.service[0].name  # Indexed for output
+  condition = var.launch_type == "ec2"
 }
 
 
-
-# FARGATE
-
-## Creates ECS Service
-########################################################################################################################
-
+# Fargate ECS Service
 resource "aws_ecs_service" "service_fargate" {
   count = var.launch_type == "fargate" ? 1 : 0
   name                               = "${var.namespace}_ECS_Service_${var.environment}"
   cluster                            = aws_ecs_cluster.default.id
-  task_definition                    = aws_ecs_task_definition.default_fargate[0].arn
+  task_definition                    = aws_ecs_task_definition.default_fargate[0].arn  # Indexed for Fargate
   desired_count                      = var.ecs_task_desired_count
   deployment_minimum_healthy_percent = var.ecs_task_deployment_minimum_healthy_percent
   deployment_maximum_percent         = var.ecs_task_deployment_maximum_percent
@@ -65,12 +59,18 @@ resource "aws_ecs_service" "service_fargate" {
   }
 
   network_configuration {
-    security_groups  = [aws_security_group.ecs_container_instance.id]
-    subnets          = aws_subnet.private.*.id
+    security_groups  = [aws_security_group.ecs_container_instance[0].id]  # Indexed for Fargate
+    subnets          = aws_subnet.private[*].id
     assign_public_ip = false
   }
 
   tags = {
     Scenario = var.scenario
   }
+}
+
+# Output for Fargate ECS Service
+output "ecs_service_fargate_name" {
+  value = aws_ecs_service.service_fargate[0].name  # Indexed for output
+  condition = var.launch_type == "fargate"
 }
