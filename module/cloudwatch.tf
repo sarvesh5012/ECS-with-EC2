62562b41546515e8b201df12667b0719aca893da
Mehul Patangya <57245913+MehulP03@users.noreply.github.com>
2024-11-05 17:01:51 +0530

    updated cloudwatch service name

--- module/cloudwatch.tf
+++ module/cloudwatch.tf
@@ -1,7 +1,8 @@
 # Create log group for our service
 
 resource "aws_cloudwatch_log_group" "log_group" {
-  name              = "/${lower(var.namespace)}/ecs/${var.service_name}"
+  for_each = var.containers
+  name              = "/${lower(var.namespace)}/ecs/${each.value.service_name}"
   retention_in_days = var.retention_in_days
 
   tags = {
