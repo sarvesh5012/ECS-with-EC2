c29c93af2a53e6ab224ac621bec391d47b401bf6
Mehul Patangya <57245913+MehulP03@users.noreply.github.com>
2024-11-05 17:33:37 +0530

    cmt-4

--- module/ecs_service.tf
+++ module/ecs_service.tf
@@ -5,20 +5,20 @@ resource "aws_ecs_service" "ec2_service" {
   name                               = "${var.namespace}_ECS_Service_${var.environment}"
   iam_role                           = aws_iam_role.ecs_service_role.arn
   cluster                            = aws_ecs_cluster.default.id
-  task_definition                    = aws_ecs_task_definition.fargate_default[0].arn  # Indexed for EC2
+  task_definition                    = aws_ecs_task_definition.fargate_default["${each.key}"].arn  # Indexed for EC2
   desired_count                      = each.value.ecs_task_desired_count
   deployment_minimum_healthy_percent = var.ecs_task_deployment_minimum_healthy_percent
   deployment_maximum_percent         = var.ecs_task_deployment_maximum_percent
   launch_type                        = var.launch_type
 
   load_balancer {
-    target_group_arn = aws_alb_target_group.ec2_service_target_group[0].arn
+    target_group_arn = aws_alb_target_group.ec2_service_target_group["${each.key}"].arn
     container_name   = each.value.service_name
     container_port   = each.value.container_port
   }
 
   network_configuration {
-    security_groups  = [aws_security_group.ecs_container_instance[0].id]  # Indexed for Fargate
+    security_groups  = [aws_security_group.ecs_container_instance["${each.key}"].id]  # Indexed for Fargate
     subnets          = aws_subnet.private[*].id
     assign_public_ip = false
   }
