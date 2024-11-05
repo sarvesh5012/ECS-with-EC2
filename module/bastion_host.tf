ee1ca0cec4fe04a956fbc7be1338bee8d0ba6362
Mehul Patangya <57245913+MehulP03@users.noreply.github.com>
2024-11-05 14:31:31 +0530

    updated basion_host ecs_autoscale

--- module/bastion_host.tf
+++ module/bastion_host.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "bastion_host" {
   instance_type               = "t3.micro"
   subnet_id                   = aws_subnet.public[0].id
   associate_public_ip_address = true
-  key_name                    = aws_key_pair.default.id
+  key_name                    = aws_key_pair.default[0].id
   vpc_security_group_ids      = [aws_security_group.bastion_host[0].id]
 
   tags = {
--- module/ecs_service_autoscaling.tf
+++ module/ecs_service_autoscaling.tf
@@ -24,9 +24,9 @@ resource "aws_appautoscaling_policy" "fargate_ecs_cpu_policy" {
   count = var.launch_type == "fargate" ? 1 : 0
   name               = "${var.namespace}_CPUTargetTrackingScaling_${var.environment}"
   policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.fargate_ecs_target.resource_id
-  scalable_dimension = aws_appautoscaling_target.fargate_ecs_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.fargate_ecs_target.service_namespace
+  resource_id        = aws_appautoscaling_target.fargate_ecs_target[0].resource_id
+  scalable_dimension = aws_appautoscaling_target.fargate_ecs_target[0].scalable_dimension
+  service_namespace  = aws_appautoscaling_target.fargate_ecs_target[0].service_namespace
 
   target_tracking_scaling_policy_configuration {
     target_value = var.cpu_target_tracking_desired_value
@@ -41,9 +41,9 @@ resource "aws_appautoscaling_policy" "ec2_ecs_cpu_policy" {
   count = var.launch_type == "ec2" ? 1 : 0
   name               = "${var.namespace}_CPUTargetTrackingScaling_${var.environment}"
   policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.ec2_ecs_target.resource_id
-  scalable_dimension = aws_appautoscaling_target.ec2_ecs_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.ec2_ecs_target.service_namespace
+  resource_id        = aws_appautoscaling_target.ec2_ecs_target[0].resource_id
+  scalable_dimension = aws_appautoscaling_target.ec2_ecs_target[0].scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ec2_ecs_target[0].service_namespace
 
   target_tracking_scaling_policy_configuration {
     target_value = var.cpu_target_tracking_desired_value
@@ -60,9 +60,9 @@ resource "aws_appautoscaling_policy" "ec2_ecs_targetecs_memory_policy" {
   count = var.launch_type == "ec2" ? 1 : 0
   name               = "${var.namespace}_MemoryTargetTrackingScaling_${var.environment}"
   policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.ec2_ecs_target.resource_id
-  scalable_dimension = aws_appautoscaling_target.ec2_ecs_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.ec2_ecs_target.service_namespace
+  resource_id        = aws_appautoscaling_target.ec2_ecs_target[0].resource_id
+  scalable_dimension = aws_appautoscaling_target.ec2_ecs_target[0].scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ec2_ecs_target[0].service_namespace
 
   target_tracking_scaling_policy_configuration {
     target_value = var.memory_target_tracking_desired_value
@@ -77,9 +77,9 @@ resource "aws_appautoscaling_policy" "fargate_ecs_memory_policy" {
   count = var.launch_type == "fargate" ? 1 : 0
   name               = "${var.namespace}_MemoryTargetTrackingScaling_${var.environment}"
   policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.fargate_ecs_target.resource_id
-  scalable_dimension = aws_appautoscaling_target.fargate_ecs_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.fargate_ecs_target.service_namespace
+  resource_id        = aws_appautoscaling_target.fargate_ecs_target[0].resource_id
+  scalable_dimension = aws_appautoscaling_target.fargate_ecs_target[0].scalable_dimension
+  service_namespace  = aws_appautoscaling_target.fargate_ecs_target[0].service_namespace
 
   target_tracking_scaling_policy_configuration {
     target_value = var.memory_target_tracking_desired_value
