dabbc795f22d0c55cc2e2c5da632f25b66795a5b
Mehul Patangya <57245913+MehulP03@users.noreply.github.com>
2024-11-05 17:07:23 +0530

    updated service ang log group

--- module/ecs_service.tf
+++ module/ecs_service.tf
@@ -5,7 +5,7 @@ resource "aws_ecs_service" "ec2_service" {
   name                               = "${var.namespace}_ECS_Service_${var.environment}"
   iam_role                           = aws_iam_role.ecs_service_role.arn
   cluster                            = aws_ecs_cluster.default.id
-  task_definition                    = aws_ecs_task_definition.ec2_default[0].arn  # Indexed for EC2
+  task_definition                    = aws_ecs_task_definition.fargate_default[0].arn  # Indexed for EC2
   desired_count                      = each.value.ecs_task_desired_count
   deployment_minimum_healthy_percent = var.ecs_task_deployment_minimum_healthy_percent
   deployment_maximum_percent         = var.ecs_task_deployment_maximum_percent
--- module/fargate_ecs_task_defination.tf
+++ module/fargate_ecs_task_defination.tf
@@ -27,7 +27,7 @@ resource "aws_ecs_task_definition" "fargate_default" {
       logConfiguration = {
         logDriver = "awslogs"
         options   = {
-          "awslogs-group"         = aws_cloudwatch_log_group.log_group.name
+          "awslogs-group"         = aws_cloudwatch_log_group.log_group[0].name
           "awslogs-region"        = var.region
           "awslogs-stream-prefix" = "${var.service_name}-log-stream-${var.environment}"
         }
