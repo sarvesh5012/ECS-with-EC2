91d78757769e0958abc59684a0584f3a1704fe48
Mehul Patangya <57245913+MehulP03@users.noreply.github.com>
2024-11-05 17:46:19 +0530

    cmt-5

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "dev-ecs-cluster-state-bkt"
-    key    = "key/terraform.tfstate"
+    key    = "key/serv/terraform.tfstate"
     region = "ap-southeast-1"
     # profile = "management-forte"
     # role_arn = "arn:aws:iam::522814693269:role/TerraformBackendAccessPolicy"
--- module/ecs_service_autoscaling.tf
+++ module/ecs_service_autoscaling.tf
@@ -10,10 +10,11 @@
 # }
 
 resource "aws_appautoscaling_target" "ec2_ecs_target" {
+  for_each = var.containers
   # count = var.launch_type == "EC2" ? 1 : 0
   max_capacity       = var.ecs_task_max_count
   min_capacity       = var.ecs_task_min_count
-  resource_id        = "service/${aws_ecs_cluster.default.name}/${aws_ecs_service.ec2_service[0].name}"
+  resource_id        = "service/${aws_ecs_cluster.default.name}/${each.value.service_name}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
 }
@@ -39,11 +40,12 @@ resource "aws_appautoscaling_target" "ec2_ecs_target" {
 
 resource "aws_appautoscaling_policy" "ec2_ecs_cpu_policy" {
   # count = var.launch_type == "EC2" ? 1 : 0
+  for_each = var.containers
   name               = "${var.namespace}_CPUTargetTrackingScaling_${var.environment}"
   policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.ec2_ecs_target.resource_id
-  scalable_dimension = aws_appautoscaling_target.ec2_ecs_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.ec2_ecs_target.service_namespace
+  resource_id        = aws_appautoscaling_target.ec2_ecs_target["${each.key}"].resource_id
+  scalable_dimension = aws_appautoscaling_target.ec2_ecs_target["${each.key}"].scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ec2_ecs_target["${each.key}"].service_namespace
 
   target_tracking_scaling_policy_configuration {
     target_value = var.cpu_target_tracking_desired_value
@@ -58,11 +60,12 @@ resource "aws_appautoscaling_policy" "ec2_ecs_cpu_policy" {
 
 resource "aws_appautoscaling_policy" "ec2_ecs_targetecs_memory_policy" {
   # count = var.launch_type == "EC2" ? 1 : 0
+  for_each = var.containers
   name               = "${var.namespace}_MemoryTargetTrackingScaling_${var.environment}"
   policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.ec2_ecs_target.resource_id
-  scalable_dimension = aws_appautoscaling_target.ec2_ecs_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.ec2_ecs_target.service_namespace
+  resource_id        = aws_appautoscaling_target.ec2_ecs_target["${each.key}"].resource_id
+  scalable_dimension = aws_appautoscaling_target.ec2_ecs_target["${each.key}"].scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ec2_ecs_target["${each.key}"].service_namespace
 
   target_tracking_scaling_policy_configuration {
     target_value = var.memory_target_tracking_desired_value
