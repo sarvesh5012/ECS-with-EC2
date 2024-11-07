37a6f169661306d0125d88abec92b8633cccf2b6
Mehul Patangya <57245913+MehulP03@users.noreply.github.com>
2024-11-07 18:07:13 +0530

    updated Launch template

--- module/launch_template.tf
+++ module/launch_template.tf
@@ -8,8 +8,8 @@ resource "aws_launch_template" "ecs_launch_template" {
   image_id               = data.aws_ami.amazon_linux_2.id
   instance_type          = var.instance_type
   key_name               = "KEY00"
-  user_data              = base64encode(data.template_file.user_data[0].rendered)
-  vpc_security_group_ids = [aws_security_group.ecs_container_instance[0].id]
+  user_data              = base64encode(data.template_file.user_data.rendered)
+  vpc_security_group_ids = [aws_security_group.ecs_container_instance[count.index].id]
 
   iam_instance_profile {
     arn = aws_iam_instance_profile.ec2_instance_role_profile.arn
@@ -25,7 +25,6 @@ resource "aws_launch_template" "ecs_launch_template" {
 }
 
 data "template_file" "user_data" {
-  count = var.launch_type == "EC2" ? 1 : 0
   template = file("module/user_data.sh")
 
   vars = {
